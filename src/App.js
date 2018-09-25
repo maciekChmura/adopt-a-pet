7859768e37c7abde2c879375e2e00d648a35f82a
Maciek Chmura <chmura.m@gmail.com>
2018-09-25 08:48:45 +0200

    handleTitleClick

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ const Pet = (props) => {
 }
 
 class App extends React.Component {
+  handleTitleClick() {
+    alert('you clicked a title')
+  }
+
   render() {
     return React.createElement('div', {}, [
-      React.createElement('h1', {}, 'Adopt a pet!'),
+      React.createElement('h1', { onClick: this.handleTitleClick }, 'Adopt a pet!'),
       React.createElement(Pet, {
         name: 'Luna',
         animal: 'dog',
