c65205a8859eeb32d2ca23dfef8c27955984cdc6
Maciek Chmura <chmura.m@gmail.com>
2018-09-27 08:32:11 +0200

    eslint plugins

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>Adopt a pet!</h1>
+        <h1>Adopt a pet!</h1>
         <Pet name="Luna" animal="dog" breed="Havanese" />
         <Pet name="Dodo" animal="bird" breed="Cockotail" />
         <Pet name="Doink" animal="cat" breed="Mixed" />
