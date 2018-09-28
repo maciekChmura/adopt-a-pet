04a53a911bebb94ac2dab66cd9e3d753fa7059e4
Maciek Chmura <chmura.m@gmail.com>
2018-09-28 08:29:03 +0200

    state + cool hack to show state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import React from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
+import { isArray } from "util";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("you clicked a title");
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .find({
+        output: "full",
+        location: "Seattle, WA"
+      })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({ pets });
+      });
   }
 
   render() {
     return (
       <div>
         <h1>Adopt a pet!</h1>
-        <Pet name="Luna" animal="dog" breed="Havanese" />
-        <Pet name="Dodo" animal="bird" breed="Cockotail" />
-        <Pet name="Doink" animal="cat" breed="Mixed" />
+        <pre>
+          <code>{JSON.stringify(this.state, null, 2)}</code>
+        </pre>
       </div>
     );
   }
