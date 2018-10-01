1928e44b6b5303f2569f4bcb26960eb1d987d099
Maciek Chmura <chmura.m@gmail.com>
2018-10-01 08:55:42 +0200

    forms and event handlers

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ANIMALS } from "petfinder-client";
 
 class SearchParams extends React.Component {
   state = {
@@ -6,16 +7,46 @@ class SearchParams extends React.Component {
     animal: "",
     breed: ""
   };
+
+  handleLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  };
+
+  handleAnimalChange = event => {
+    this.setState({
+      animal: event.target.value
+    });
+  };
+
   render() {
     return (
       <div className="search-params">
         <label htmlFor="location">
           Location
           <input
+            onChange={this.handleLocationChange}
             id="location"
             value={this.state.location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={this.state.animal}
+            onChange={this.handleAnimalChange}
+            onBlur={this.handleAnimalChange}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
       </div>
     );
